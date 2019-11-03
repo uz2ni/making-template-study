1239f50e7b60bb577d6b4e553542ce70c3aa0ce3
Mary <dlalswjd95@naver.com>
2019-11-03 18:37:12 +0900

    previewFrame 기본 레이아웃 생성

--- components/editor/preview-frame.vue
+++ components/editor/preview-frame.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>preview frame</div>
+  <div class="preview_wrap">
+    <div class="preview_con">
+      <div class="preview_head">
+        <h1 class="tit">Preview</h1>
+      </div>
+      <div class="preview_body">
+        <!--여기에 구성요소 추가-->
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
   export default {
     name: "preview-frame"
@@ -9,5 +17,25 @@
 </script>
 
 <style scoped>
-
+  .preview_wrap {
+    width: 396px;
+    height: 726px;
+    margin-left: 20px;
+    border: 2px solid #dadada;
+    padding: 0 12px;
+    border-radius: 35px;
+    background-color: #ebebeb;
+  }
+  .preview_head{
+    height: 68px;
+    padding: 20px 0;
+  }
+  .preview_head .tit {
+    text-align: center;
+  }
+  .preview_body{
+    background-color: #fff;
+    height: 617px;
+    border: 2px solid #ddd;
+  }
 </style>
