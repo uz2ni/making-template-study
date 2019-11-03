3c4ec664df2523eeec699cadc9b3ea6765af1705
yujin song <uzini_@likelion.org>
2019-11-03 17:26:43 +0900

    [header, footer] title, url, button 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="editor-wrap">
-    <header>header</header>
+    <header>
+      <div class="cont-wrap">
+        <div class="cont-tit">타이틀 타이틀 타이틀 타이틀</div>
+        <div class="cont-url">http://urlname.i-on.net</div>
+      </div>
+    </header>
     <section>
       <component-bar></component-bar> <!--chloe-->
       <preview-frame></preview-frame> <!--mary-->
       <edit-frame></edit-frame> <!--daisy-->
     </section>
-    <footer>footer</footer>
+    <footer>
+      <div class="btn-wrap">
+        <a class="btn" @click="save">저장</a>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -19,13 +28,73 @@ export default {
     ComponentBar,
     PreviewFrame,
     EditFrame,
+  },
+  methods: {
+    save() {
+      alert('save test');
+    }
   }
 }
 </script>
 
 <style scoped>
-  .editor-wrap {
-
+  .editor-wrap * {
+    /*border: 1px solid black;*/
   }
+  /*********** header ***********/
+  header {
+    height: 10vh;
+    background-color: #303437;
+    display: flex;
+    align-items: center;
+  }
+  header > .cont-wrap {
+    display: flex;
+    margin-left: 15px;
+  }
+  header > .cont-wrap > div {
+  }
+  header > .cont-wrap > .cont-tit {
+    color: white;
+    font-size: 1.3rem;
+  }
+  header > .cont-wrap > .cont-url {
+    color: #868686;
+    margin-left: 15px;
+  }
+
+  /*********** section ***********/
+  section {
+    height: 80vh;
+  }
+  /*********** footer ***********/
+  footer {
+    height: 10vh;
+    background-color: #e7e7e7;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  /*********** button ***********/
+  .btn-wrap {
+    width: 50vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .btn {
+    width: 100px;
+    height: 35px;
+    text-align: center;
+    line-height: 35px;
+    border-radius: 30px;
+    background-color: #94C2ED; /* 버튼색 임시 */
+    color: white;
+    font-size: 0.9rem;
+    cursor:pointer;
+    font-weight: bold;
+  }
+
 </style>
 
