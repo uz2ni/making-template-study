5e8bd790245686f1085cae78d7264f06a2113fc0
chloe <chloe@mail.i-on.net>
2019-12-09 03:12:19 +0900

    컴포넌트 바 쪼개기, 토글 애니메이션 추가

--- components/editor/component-bar.vue
+++ components/editor/component-bar.vue
@@ -3,52 +3,52 @@
     <p class="sideBarTit">구성요소 추가</p>
     <ul class="sideBarlist1">
       <li>
-        <a href="#" @click.prevent="active1 = !active1">텍스트<span class="arrowDown" v-show="!active1"></span><span class="arrowUp" v-show="active1"></span></a>
-        <ul class="sideBarlist2" v-show="active1">
-          <li><a href="#">타이틀 텍스트<span class="off"></span></a></li>
-          <li><a href="#">본문형 텍스트<span class="off"></span></a></li>
-          <li><a href="#">목록형 텍스트<span class="off"></span></a></li>
+        <li-comp type="liDepth1">타이틀</li-comp>
+        <ul class="sideBarlist2">
+          <li><li-comp type="liDepth2">타이틀 텍스트</li-comp></li>
+          <li><li-comp type="liDepth2">본문형 텍스트</li-comp></li>
+          <li><li-comp type="liDepth2">목록형 텍스트</li-comp></li>
         </ul>
       </li>
       <li>
-        <a href="#" @click.prevent="active2 = !active2">이미지<span class="arrowDown" v-show="!active2"></span><span class="arrowUp" v-show="active2"></span></a>
-        <ul class="sideBarlist2" v-show="active2">
-          <li><a href="#">이미지<span class="off"></span></a></li>
-          <li><a href="#">텍스트 + 이미지<span class="off"></span></a></li>
-          <li><a href="#">좌측이미지 + 텍스트<span class="off"></span></a></li>
-          <li><a href="#">우측이미지 + 텍스트<span class="off"></span></a></li>
+        <li-comp type="liDepth1">이미지</li-comp>
+        <ul class="sideBarlist2">
+          <li><li-comp type="liDepth2">이미지</li-comp></li>
+          <li><li-comp type="liDepth2">텍스트 + 이미지</li-comp></li>
+          <li><li-comp type="liDepth2">좌측이미지 + 텍스트</li-comp></li>
+          <li><li-comp type="liDepth2">우측이미지 + 텍스트</li-comp></li>
         </ul>
       <li>
-        <a href="#" @click.prevent="active3 = !active3">지도<span class="arrowDown" v-show="!active3"></span><span class="arrowUp" v-show="active3"></span></a>
-        <ul class="sideBarlist2" v-show="active3">
-          <li><a href="#">지도<span class="off"></span></a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="#" @click.prevent="active4 = !active4">버튼<span class="arrowDown" v-show="!active4"></span><span class="arrowUp" v-show="active4"></span></a>
-        <ul class="sideBarlist2" v-show="active4">
-          <li><a href="#">네모버튼<span class="off"></span></a></li>
-          <li><a href="#">막대버튼<span class="off"></span></a></li>
+        <li-comp type="liDepth1">지도</li-comp>
+        <ul class="sideBarlist2">
+          <li><li-comp type="liDepth2">지도</li-comp></li>
         </ul>
       </li>
       <li>
-        <a href="#" @click.prevent="active5 = !active5">표<span class="arrowDown" v-show="!active5"></span><span class="arrowUp" v-show="active5"></span></a>
-        <ul class="sideBarlist2" v-show="active5">
-          <li><a href="#">표<span class="off"></span></a></li>
+        <li-comp type="liDepth1">버튼</li-comp>
+        <ul class="sideBarlist2">
+          <li><li-comp type="liDepth2">네모버튼</li-comp></li>
+          <li><li-comp type="liDepth2">막대버튼</li-comp></li>
         </ul>
       </li>
       <li>
-        <a href="#" @click.prevent="active6 = !active6">동영상<span class="arrowDown" v-show="!active6"></span><span class="arrowUp" v-show="active6"></span></a>
-        <ul class="sideBarlist2" v-show="active6">
-          <li><a href="#">동영상주소<span class="off"></span></a></li>
+        <li-comp type="liDepth1">표</li-comp>
+        <ul class="sideBarlist2">
+          <li><li-comp type="liDepth2">표</li-comp></li>
         </ul>
       </li>
       <li>
-        <a href="#" @click.prevent="active7 = !active7">구분선<span class="arrowDown" v-show="!active7"></span><span class="arrowUp" v-show="active7"></span></a>
-        <ul class="sideBarlist2" v-show="active7">
-          <li><a href="#">공백<span class="off"></span></a></li>
-          <li><a href="#">실선<span class="off"></span></a></li>
-          <li><a href="#">점선<span class="off"></span></a></li>
+        <li-comp type="liDepth1">동영상</li-comp>
+        <ul class="sideBarlist2">
+          <li><li-comp type="liDepth2">동영상주소</li-comp></li>
+        </ul>
+      </li>
+      <li>
+        <li-comp type="liDepth1">구분선</li-comp>
+        <ul class="sideBarlist2">
+          <li><li-comp type="liDepth2">공백</li-comp></li>
+          <li><li-comp type="liDepth2">실선</li-comp></li>
+          <li><li-comp type="liDepth2">점선</li-comp></li>
         </ul>
       </li>
     </ul>
@@ -57,31 +57,26 @@
 
 <script type="JavaScript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js"></script>
 <script>
+  import liComp from '~/components/common/li-comp.vue'
   export default {
-    name: "component-bar",
-    data:function() {
-      return {
-        active1: false,
-        active2: false,
-        active3: false,
-        active4: false,
-        active5: false,
-        active6: false,
-        active7: false,
-      }
-    }
+    components: {
+      'liComp' : liComp
+    },
+    name: "component-bar"
   }
 
 </script>
 
-<style scoped>
-#sideBar { position:relative; width:200px; min-height:500px; background:#fff; margin:20px; border:1px solid #d7d7d7;}
-.sideBarTit { position:relative; width:100%; height:40px; background:#eee; padding-left:15px; line-height:40px; }
+<style lang="css" scoped>
+
+#sideBar { position:absolute; width:200px; min-height:500px; background:#fff; margin-right:20px; border:1px solid #d7d7d7;}
+.sideBarTit { position:relative; width:100%; height:44px; background:#eee; padding-left:15px; line-height:44px; font-weight:600; }
 .sideBarlist1,.sideBarlist2  { position:relative; width:100%; height:auto; }
-.sideBarlist1 > li { position:relative; width:100%; border-bottom:1px solid #e5e5e5; line-height:40px;  }
-.sideBarlist1 > li > a { position:relative; display:inline-block; width:100%; color:#000; text-decoration:none; padding-left:15px;}
-.sideBarlist1 > li > a > span { position:absolute; right:30px; top:12px; display:inline-block; width:15px;
-  height:15px; }
+.sideBarlist1 > li { position:relative; width:100%; border-bottom:1px solid #e5e5e5; line-height:40px; }
+.sideBarlist1 > li > ul { max-height:0; overflow:hidden; transition: all 0.5s ease-out -0.3s;}
+.sideBarlist1 > li > a { position:relative; display:inline-block; width:100%; color:#000; text-decoration:none; padding-left:15px; font-size:13px;}
+.sideBarlist1 > li > a.on + ul.sideBarlist2 { max-height:1000px; transition: all 0.5s ease-in; }
+.sideBarlist1 > li > a > span { position:absolute; right:30px; top:12px; display:inline-block; width:15px; height:15px; }
 .sideBarlist1 > li > a > span.arrowDown { background:url('~@/assets/images/arrowDown.png') no-repeat no-repeat center; background-size:15px; }
 .sideBarlist1 > li > a > span.arrowUp { background:url('~@/assets/images/arrowUp.png') no-repeat no-repeat center; background-size:15px; }
 .sideBarlist2 > li { position:relative; width:100%; border-top:1px solid #e5e5e5; line-height:40px; background:#f6f7f7; }
@@ -89,6 +84,5 @@
 .sideBarlist2 > li > a > span { position:absolute; right:30px; top:12px; display:inline-block; width:15px; height:15px; }
 .sideBarlist2 > li > a > span.on { background:url('~@/assets/images/plusOn.png') no-repeat no-repeat center; background-size:12px; }
 .sideBarlist2 > li > a > span.off { background:url('~@/assets/images/plusOff.png') no-repeat no-repeat center; background-size:12px; }
-
 
 </style>
