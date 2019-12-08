705306f034d2341a64f1865bcd83e5480c336bf6
chloe <chloe@mail.i-on.net>
2019-12-09 03:16:44 +0900

    컴포넌트 바 쪼개기, 토글 애니메이션 추가

---
+++ components/common/li-comp.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+  <!--1Depth list item 화살표형-->
+  <a v-if="type=='liDepth1'" v-bind:class="{on: isActive}" @click.prevent="dropToggle">
+    <slot></slot><span v-bind:class="{arrowUp : isActive, arrowDown : !isActive}"></span>
+  </a>
+  <!--//1Depth list item-->
+  <a v-else-if="type=='liDepth2'" @click.prevent="addItem">
+    <slot></slot><span v-bind:class="{ added : isAdded, unAdded : !isAdded}"></span>
+  </a>
+
+</template>
+
+<script>
+export default {
+  name:"li-comp",
+  props:['type'],
+  data: function(){
+    return {
+      dropState: false,
+      isActive:false,
+      isAdded:false
+    }
+  },
+  methods: {
+    dropToggle () {
+      if(this.isActive){
+        console.log("off");
+        this.isActive = !this.isActive;
+      }else{
+        console.log("on");
+        this.isActive = !this.isActive;
+      }
+    },
+    addItem () { //아이템 추가방식 고려 후 수정 필요
+      if(this.isAdded){
+        console.log("Item Add Stop!");
+        this.isAdded = !this.isAdded;
+      }else{
+        console.log("Item Added!");
+        this.isAdded = !this.isAdded;
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+span.arrowDown { background:url('~@/assets/images/arrowDown.png') no-repeat no-repeat center; background-size:15px; }
+span.arrowUp { background:url('~@/assets/images/arrowUp.png') no-repeat no-repeat center; background-size:15px; }
+a > span { position:absolute; right:30px; top:12px; display:inline-block; width:15px; height:15px; }
+span.added { background:url('~@/assets/images/plusOn.png') no-repeat no-repeat center; background-size:12px; }
+span.unAdded { background:url('~@/assets/images/plusOff.png') no-repeat no-repeat center; background-size:12px; }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,29 @@
       </div>
     </header>
     <section>
-      <component-bar></component-bar> <!--chloe-->
-      <preview-frame></preview-frame> <!--mary-->
-      <edit-frame></edit-frame> <!--daisy-->
+      <!-- 페이지 추가 영역 -->
+      <div class="page-view">
+        <div v-show="isShowList">
+          <div class="page-view-list"> <!-- 페이지 추가 리스트 -->
+            <div class="page-view-col" v-for="(page,index) in pageList" v-bind:key="index">
+              <span>{{ page.title }}</span>
+              <button @click="removePage(index)">X</button>
+            </div>
+          </div>
+          <div class="page-add-area"> <!-- 페이지 추가 버튼 -->
+            <button @click="addPage">페이지 추가</button>
+          </div>
+        </div>
+        <div class="page-view-toggle"> <!-- 보이기/숨기기 -->
+          <button @click="listToggle">{{ toggleTitle }}</button>
+        </div>
+      </div>
+      <!-- //페이지 추가 영역 -->
+      <div class="edit-area">
+        <component-bar></component-bar> <!--chloe-->
+        <preview-frame></preview-frame> <!--mary-->
+        <edit-frame></edit-frame> <!--daisy-->
+      </div>
     </section>
     <footer>
       <div class="btn-wrap">
@@ -29,9 +49,31 @@ export default {
     PreviewFrame,
     EditFrame,
   },
+  data() {
+    return {
+      pageList: [
+        { title: "홈" },
+        { title: "메뉴" },
+        { title: "게시판" },
+        { title: "오시는길" }
+      ],
+      toggleTitle: "숨기기",
+      isShowList: true
+    }
+  },
   methods: {
     save() {
       alert('save test');
+    },
+    addPage() { // 페이지 리스트 추가
+      this.pageList.push({ title: "리스트추가["+this.pageList.length+"]" });
+    },
+    removePage(index) { // 페이지 리스트 삭제
+      this.pageList.splice(index,1);
+    },
+    listToggle() { // 페이지 리스트 Show/Hide
+      this.isShowList = !this.isShowList;
+      this.toggleTitle = (this.isShowList ? "숨기기" : "보이기");
     }
   }
 }
@@ -67,18 +109,52 @@ export default {
   section {
     height: 80vh;
   }
-  /*********** footer ***********/
-  footer {
-    height: 10vh;
-    background-color: #e7e7e7;
+  section > .edit-area {
+    display: flex;
+    justify-content: space-between;
+  }
+  section > .page-view > div {
+    display: flex;
+  }
+  section > .page-view > div > .page-view-list {
+    width: 90%;
+    height: 200px;
+    display: flex;
+    border: 1px solid black;
+  }
+  section > .page-view > div > .page-view-list > .page-view-col {
+    border: 1px solid black;
+    width: 130px;
+  }
+  section > .page-view > div > .page-add-area {
+    border: 1px solid black;
+    width: 10%;
     display: flex;
     justify-content: center;
     align-items: center;
   }
+  section > .page-view > .page-view-toggle {
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  section > .page-view > .page-view-toggle > button {
+    margin: 5px;
+  }
+  /*********** footer ***********/
+  footer {
+    width:100%;
+    height: 10vh;
+    background-color: #e7e7e7;
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    bottom: 0;
+  }
 
   /*********** button ***********/
   .btn-wrap {
-    width: 50vh;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -97,4 +173,3 @@ export default {
   }
 
 </style>
-
